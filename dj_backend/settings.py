42b5d6d4dddf386579bdded7782e61d145fc681a
Ankan Giri <ankangiri05@gmail.com>
2025-09-19 22:24:25 +0530

    Third Commit

--- dj_backend/settings.py
+++ dj_backend/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
-import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4mjfycc=-z1l@ld9q3)6ggu7sox*91e2kz$f$ajrsl-i(^yh)5'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
@@ -81,22 +83,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dj_backend.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "sehatsaarthi_db",
-        "USER": "root",
-        "PASSWORD": "oocRIcPkqKWpMdNTegVfrKpjOeHbllnC",
-        "HOST": "maglev.proxy.rlwy.net",
-        "PORT": "34579",
+        "NAME": os.getenv('DATABASE_NAME'),
+        "USER": os.getenv('DATABASE_USER'),
+        "PASSWORD": os.getenv('DATABASE_PASSWORD'),
+        "HOST": os.getenv('DATABASE_HOST'),
+        "PORT": os.getenv('DATABASE_PORT'),
     }
-    # "default": dj_database_url.config(
-    #     default="mysql://root:oocRIcPkqKWpMdNTegVfrKpjOeHbllnC@maglev.proxy.rlwy.net:34579/railway",
-    # )
 }
 
 
@@ -145,17 +143,3 @@ STATIC_ROOT = BASE_DIR / 'staticfiles' # For production
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'DEBUG',
-    },
-}
